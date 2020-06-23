b8d68ec56ed2ac5e5a2376c818c54e9fe6516090
Antti Kaihola <antti.kaihola@eniram.fi>
2020-06-23 23:40:26 +0300

    Refactor `pg_split_schema_dump.py`
    Create a main function so we can create a console entry point.

--- pgtricks/pg_split_schema_dump.py
+++ pgtricks/pg_split_schema_dump.py
@@ -10,7 +10,7 @@ value which was active in the original dump at that point.
 Usage::
 
     pg_dump -s -f my_schema_dump.sql my_database_name
-    python pg_split_schema_dump.py my_schema_dump.sql /my/dump/directory
+    pg_split_schema_dump my_schema_dump.sql /my/dump/directory
 
 """
 
@@ -18,48 +18,62 @@ import os
 import re
 import sys
 
-sqlpath = sys.argv[1]
-target_directory = sys.argv[2]
-
-if not os.path.isdir(target_directory):
-    os.mkdir(target_directory)
-
-sql = open(sqlpath).read()
-
-SPLIT_RE = re.compile(r'''
+SPLIT_RE = re.compile(
+    r'''
 
 --
--- ''')
+-- '''
+)
 
 
-IDENTIFY_RE = re.compile(r'''
+IDENTIFY_RE = re.compile(
+    r'''
 
 --
 -- Name: ([^\n;]+?)(?:\([^)]*\))?; Type: ([^\n;]+); Schema: ([^\n;]+?); Owner:[^\n]*
 --
 
-''', re.DOTALL | re.MULTILINE)
+''',
+    re.DOTALL | re.MULTILINE,
+)
 
 SEARCH_PATH_RE = re.compile(r'SET search_path = [^\n;]+;')
-search_path = 'SET search_path = public;'
 
-parts = [match.start() for match in SPLIT_RE.finditer(sql)]
-for start, end in zip(parts[:-1], parts[1:]):
-    part = sql[start:end]
-    match = IDENTIFY_RE.match(part)
-    if not match:
-        print(part)
-        sys.exit(0)
-    name = match.group(1).replace(' ', '_')
-    type_ = match.group(2).replace(' ', '_')
-    schema = match.group(3)
-    if schema == '-':
-        schema = 'no_schema'
-    if name == 'id':
-        continue
-    dump_filename = '{}.{}.{}'.format(schema, name, type_)
-    dump_filepath = os.path.join(target_directory, dump_filename)
-    open(dump_filepath, 'w').write('{}\n\n{}'.format(search_path, part))
-    search_path_match = SEARCH_PATH_RE.search(part)
-    if search_path_match:
-        search_path = search_path_match.group()
+
+def split_sql_file(sqlpath, target_directory):
+    if not os.path.isdir(target_directory):
+        os.mkdir(target_directory)
+
+    sql = open(sqlpath).read()
+
+    search_path = "SET search_path = public;"
+
+    parts = [match.start() for match in SPLIT_RE.finditer(sql)]
+    for start, end in zip(parts[:-1], parts[1:]):
+        part = sql[start:end]
+        match = IDENTIFY_RE.match(part)
+        if not match:
+            print(part)
+            sys.exit(0)
+        name = match.group(1).replace(" ", "_")
+        type_ = match.group(2).replace(" ", "_")
+        schema = match.group(3)
+        if schema == "-":
+            schema = "no_schema"
+        if name == "id":
+            continue
+        dump_filename = "{}.{}.{}".format(schema, name, type_)
+        dump_filepath = os.path.join(target_directory, dump_filename)
+        open(dump_filepath, "w").write("{}\n\n{}".format(search_path, part))
+        search_path_match = SEARCH_PATH_RE.search(part)
+        if search_path_match:
+            search_path = search_path_match.group()
+
+
+def main(args=None):
+    sqlpath, target_directory = sys.argv[1:] if args is None else args
+    split_sql_file(sqlpath, target_directory)
+
+
+if __name__ == "__main__":
+    main()
