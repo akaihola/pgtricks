34a292d9d81edace6dfe658f00f57ccbc65206c6
Antti Kaihola <antti.kaihola@eniram.fi>
2021-09-12 11:49:08 +0300

    Add merge sort tool

---
+++ pgtricks/mergesort.py
@@ -0,0 +1,60 @@
+"""Merge sort implementation to handle large files by sorting them in partitions."""
+
+from __future__ import annotations
+
+import sys
+from heapq import merge
+from tempfile import TemporaryFile
+from typing import IO, Iterable, Iterator, cast
+
+
+class MergeSort(Iterable[str]):
+    """Merge sort implementation to handle large files by sorting them in partitions."""
+
+    def __init__(self, directory: str = ".", max_memory: int = 190) -> None:
+        """Initialize the merge sort object."""
+        self._directory = directory
+        self._max_memory = max_memory
+        self._partitions: list[IO[str]] = []
+        self._iterating: Iterable[str] | None = None
+        self._buffer: list[str] = []
+        self._memory_counter = 0
+        self._flush()
+
+    def append(self, line: str) -> None:
+        """Append a line to the set of lines to be sorted."""
+        if self._iterating:
+            message = "Can't append lines after starting to sort"
+            raise ValueError(message)
+        self._memory_counter -= sys.getsizeof(self._buffer)
+        self._buffer.append(line)
+        self._memory_counter += sys.getsizeof(self._buffer)
+        self._memory_counter += sys.getsizeof(line)
+        if self._memory_counter >= self._max_memory:
+            self._flush()
+
+    def _flush(self) -> None:
+        if self._buffer:
+            self._partitions.append(TemporaryFile(mode="w+", dir=self._directory))
+            self._partitions[-1].writelines(sorted(self._buffer))
+        self._buffer = []
+        self._memory_counter = sys.getsizeof(self._buffer)
+
+    def __next__(self) -> str:
+        """Return the next line in the sorted list of lines."""
+        if not self._iterating:
+            if self._partitions:
+                # At least one partition has already been flushed to disk.
+                # Iterate the merge sort for all partitions.
+                self._flush()
+                for partition in self._partitions:
+                    partition.seek(0)
+                self._iterating = merge(*self._partitions)
+            else:
+                # All lines fit in memory. Iterate the list of lines directly.
+                self._iterating = iter(sorted(self._buffer))
+        return next(cast(Iterator[str], self._iterating))
+
+    def __iter__(self) -> Iterator[str]:
+        """Return the iterator object for the sorted list of lines."""
+        return self
---
+++ pgtricks/tests/test_mergesort.py
@@ -0,0 +1,83 @@
+"""Tests for the `pgtricks.mergesort` module."""
+
+from types import GeneratorType
+from typing import Iterable, cast
+
+import pytest
+
+from pgtricks.mergesort import MergeSort
+
+
+def test_mergesort_append(tmpdir):
+    """Test appending lines to the merge sort object."""
+    m = MergeSort(directory=tmpdir, max_memory=190)
+    m.append("1\n")
+    assert m._buffer == ["1\n"]
+    m.append("2\n")
+    assert m._buffer == []
+    m.append("3\n")
+    assert m._buffer == ["3\n"]
+    assert len(m._partitions) == 1
+    assert m._partitions[0].tell() == len("1\n2\n")
+    m._partitions[0].seek(0)
+    assert m._partitions[0].read() == "1\n2\n"
+
+
+def test_mergesort_flush(tmpdir):
+    """Test flushing the buffer to disk."""
+    m = MergeSort(directory=tmpdir, max_memory=190)
+    for value in [1, 2, 3]:
+        m.append(f"{value}\n")
+    m._flush()
+    assert len(m._partitions) == 2
+    assert m._partitions[0].tell() == len("1\n2\n")
+    m._partitions[0].seek(0)
+    assert m._partitions[0].read() == "1\n2\n"
+    assert m._partitions[1].tell() == len("3\n")
+    m._partitions[1].seek(0)
+    assert m._partitions[1].read() == "3\n"
+
+
+def test_mergesort_iterate_disk(tmpdir):
+    """Test iterating over the sorted lines on disk."""
+    m = MergeSort(directory=tmpdir, max_memory=190)
+    for value in [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 8, 4]:
+        m.append(f"{value}\n")
+    assert next(m) == "1\n"
+    assert isinstance(m._iterating, GeneratorType)
+    assert next(m) == "1\n"
+    assert next(m) == "2\n"
+    assert next(m) == "3\n"
+    assert next(m) == "3\n"
+    assert next(m) == "4\n"
+    assert next(m) == "4\n"
+    assert next(m) == "5\n"
+    assert next(m) == "5\n"
+    assert next(m) == "6\n"
+    assert next(m) == "8\n"
+    assert next(m) == "9\n"
+    with pytest.raises(StopIteration):
+        next(m)
+
+
+def test_mergesort_iterate_memory(tmpdir):
+    """Test iterating over the sorted lines when all lines fit in memory."""
+    m = MergeSort(directory=tmpdir, max_memory=1000000)
+    for value in [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 8, 4]:
+        m.append(f"{value}\n")
+    assert next(m) == "1\n"
+    assert not isinstance(m._iterating, GeneratorType)
+    assert iter(cast(Iterable[str], m._iterating)) is m._iterating
+    assert next(m) == "1\n"
+    assert next(m) == "2\n"
+    assert next(m) == "3\n"
+    assert next(m) == "3\n"
+    assert next(m) == "4\n"
+    assert next(m) == "4\n"
+    assert next(m) == "5\n"
+    assert next(m) == "5\n"
+    assert next(m) == "6\n"
+    assert next(m) == "8\n"
+    assert next(m) == "9\n"
+    with pytest.raises(StopIteration):
+        next(m)
