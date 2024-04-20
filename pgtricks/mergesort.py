d746c2d53ee97e57567c3a4a0b87efa000d4dbf9
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-04-20 11:22:58 +0300

    Stop newline conversion on Win. Test all newlines.

--- pgtricks/mergesort.py
+++ pgtricks/mergesort.py
@@ -21,7 +21,8 @@ class MergeSort(Iterable[str]):
         self._key = key
         self._directory = directory
         self._max_memory = max_memory
-        self._partitions: list[IO[str]] = []
+        # Use binary mode to avoid newline conversion on Windows.
+        self._partitions: list[IO[bytes]] = []
         self._iterating: Iterable[str] | None = None
         self._buffer: list[str] = []
         self._memory_counter = 0
@@ -41,8 +42,11 @@ class MergeSort(Iterable[str]):
 
     def _flush(self) -> None:
         if self._buffer:
-            self._partitions.append(TemporaryFile(mode="w+", dir=self._directory))
-            self._partitions[-1].writelines(sorted(self._buffer, key=self._key))
+            # Use binary mode to avoid newline conversion on Windows.
+            self._partitions.append(TemporaryFile(mode="w+b", dir=self._directory))
+            self._partitions[-1].writelines(
+                line.encode("UTF-8") for line in sorted(self._buffer, key=self._key)
+            )
         self._buffer = []
         self._memory_counter = sys.getsizeof(self._buffer)
 
@@ -55,7 +59,13 @@ class MergeSort(Iterable[str]):
                 self._flush()
                 for partition in self._partitions:
                     partition.seek(0)
-                self._iterating = merge(*self._partitions, key=self._key)
+                self._iterating = merge(
+                    *[
+                        (line.decode("UTF-8") for line in partition)
+                        for partition in self._partitions
+                    ],
+                    key=self._key,
+                )
             else:
                 # All lines fit in memory. Iterate the list of lines directly.
                 self._iterating = iter(sorted(self._buffer))
--- pgtricks/tests/test_mergesort.py
+++ pgtricks/tests/test_mergesort.py
@@ -1,6 +1,5 @@
 """Tests for the `pgtricks.mergesort` module."""
 
-import os
 from types import GeneratorType
 from typing import Iterable, cast
 
@@ -8,90 +7,97 @@ import pytest
 
 from pgtricks.mergesort import MergeSort
 
-LF = os.linesep
+# This is the biggest amount of memory which can't hold two one-character lines on any
+# platform. On Windows it's slightly smaller than on Unix.
+JUST_BELOW_TWO_SHORT_LINES = 174
 
 
-def test_mergesort_append(tmpdir):
+@pytest.mark.parametrize("lf", ["\n", "\r\n"])
+def test_mergesort_append(tmpdir, lf):
     """Test appending lines to the merge sort object."""
-    m = MergeSort(directory=tmpdir, max_memory=190)
-    m.append(f"1{LF}")
-    assert m._buffer == [f"1{LF}"]
-    m.append(f"2{LF}")
+    m = MergeSort(directory=tmpdir, max_memory=JUST_BELOW_TWO_SHORT_LINES)
+    m.append(f"1{lf}")
+    assert m._buffer == [f"1{lf}"]
+    m.append(f"2{lf}")
     assert m._buffer == []
-    m.append(f"3{LF}")
-    assert m._buffer == [f"3{LF}"]
+    m.append(f"3{lf}")
+    assert m._buffer == [f"3{lf}"]
     assert len(m._partitions) == 1
     pos = m._partitions[0].tell()
     m._partitions[0].seek(0)
-    assert m._partitions[0].read() == f"1{LF}2{LF}"
-    assert pos == len(f"1{LF}2{LF}")
+    assert m._partitions[0].read() == f"1{lf}2{lf}".encode("UTF-8")
+    assert pos == len(f"1{lf}2{lf}")
 
 
-def test_mergesort_flush(tmpdir):
+@pytest.mark.parametrize("lf", ["\n", "\r\n"])
+def test_mergesort_flush(tmpdir, lf):
     """Test flushing the buffer to disk."""
-    m = MergeSort(directory=tmpdir, max_memory=190)
+    m = MergeSort(directory=tmpdir, max_memory=JUST_BELOW_TWO_SHORT_LINES)
     for value in [1, 2, 3]:
-        m.append(f"{value}{LF}")
+        m.append(f"{value}{lf}")
     m._flush()
     assert len(m._partitions) == 2
-    assert m._partitions[0].tell() == len(f"1{LF}2{LF}")
+    assert m._partitions[0].tell() == len(f"1{lf}2{lf}")
     m._partitions[0].seek(0)
-    assert m._partitions[0].read() == f"1{LF}2{LF}"
+    assert m._partitions[0].read() == f"1{lf}2{lf}".encode("UTF-8")
     pos = m._partitions[1].tell()
     m._partitions[1].seek(0)
-    assert m._partitions[1].read() == f"3{LF}"
-    assert pos == len(f"3{LF}")
+    assert m._partitions[1].read() == f"3{lf}".encode("UTF-8")
+    assert pos == len(f"3{lf}")
 
 
-def test_mergesort_iterate_disk(tmpdir):
+@pytest.mark.parametrize("lf", ["\n", "\r\n"])
+def test_mergesort_iterate_disk(tmpdir, lf):
     """Test iterating over the sorted lines on disk."""
-    m = MergeSort(directory=tmpdir, max_memory=190)
+    m = MergeSort(directory=tmpdir, max_memory=JUST_BELOW_TWO_SHORT_LINES)
     for value in [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 8, 4]:
-        m.append(f"{value}{LF}")
-    assert next(m) == f"1{LF}"
+        m.append(f"{value}{lf}")
+    assert next(m) == f"1{lf}"
     assert isinstance(m._iterating, GeneratorType)
-    assert next(m) == f"1{LF}"
-    assert next(m) == f"2{LF}"
-    assert next(m) == f"3{LF}"
-    assert next(m) == f"3{LF}"
-    assert next(m) == f"4{LF}"
-    assert next(m) == f"4{LF}"
-    assert next(m) == f"5{LF}"
-    assert next(m) == f"5{LF}"
-    assert next(m) == f"6{LF}"
-    assert next(m) == f"8{LF}"
-    assert next(m) == f"9{LF}"
+    assert next(m) == f"1{lf}"
+    assert next(m) == f"2{lf}"
+    assert next(m) == f"3{lf}"
+    assert next(m) == f"3{lf}"
+    assert next(m) == f"4{lf}"
+    assert next(m) == f"4{lf}"
+    assert next(m) == f"5{lf}"
+    assert next(m) == f"5{lf}"
+    assert next(m) == f"6{lf}"
+    assert next(m) == f"8{lf}"
+    assert next(m) == f"9{lf}"
     with pytest.raises(StopIteration):
         next(m)
 
 
-def test_mergesort_iterate_memory(tmpdir):
+@pytest.mark.parametrize("lf", ["\n", "\r\n"])
+def test_mergesort_iterate_memory(tmpdir, lf):
     """Test iterating over the sorted lines when all lines fit in memory."""
     m = MergeSort(directory=tmpdir, max_memory=1000000)
     for value in [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 8, 4]:
-        m.append(f"{value}{LF}")
-    assert next(m) == f"1{LF}"
+        m.append(f"{value}{lf}")
+    assert next(m) == f"1{lf}"
     assert not isinstance(m._iterating, GeneratorType)
     assert iter(cast(Iterable[str], m._iterating)) is m._iterating
-    assert next(m) == f"1{LF}"
-    assert next(m) == f"2{LF}"
-    assert next(m) == f"3{LF}"
-    assert next(m) == f"3{LF}"
-    assert next(m) == f"4{LF}"
-    assert next(m) == f"4{LF}"
-    assert next(m) == f"5{LF}"
-    assert next(m) == f"5{LF}"
-    assert next(m) == f"6{LF}"
-    assert next(m) == f"8{LF}"
-    assert next(m) == f"9{LF}"
+    assert next(m) == f"1{lf}"
+    assert next(m) == f"2{lf}"
+    assert next(m) == f"3{lf}"
+    assert next(m) == f"3{lf}"
+    assert next(m) == f"4{lf}"
+    assert next(m) == f"4{lf}"
+    assert next(m) == f"5{lf}"
+    assert next(m) == f"5{lf}"
+    assert next(m) == f"6{lf}"
+    assert next(m) == f"8{lf}"
+    assert next(m) == f"9{lf}"
     with pytest.raises(StopIteration):
         next(m)
 
 
-def test_mergesort_key(tmpdir):
+@pytest.mark.parametrize("lf", ["\n", "\r\n"])
+def test_mergesort_key(tmpdir, lf):
     """Test sorting lines based on a key function."""
     m = MergeSort(directory=tmpdir, key=lambda line: -int(line[0]))
     for value in [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 8, 4]:
-        m.append(f"{value}{LF}")
+        m.append(f"{value}{lf}")
     result = "".join(value[0] for value in m)
     assert result == "986554433211"
